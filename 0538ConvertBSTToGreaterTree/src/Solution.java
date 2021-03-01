d37a71c2aba86d770782714f2afc62984620563a
baimohan <csbaimohan@foxmail.com>
2021-03-01 16:58:00 +0800

    leetcode 0538

---
+++ 0538ConvertBSTToGreaterTree/src/Solution.java
@@ -0,0 +1,46 @@
+/**
+ * 类描述：
+ *
+ * @ClassName Solution
+ * @Description TODO
+ * @Author BaiMohan
+ * @Date 2021/3/1 16:50
+ * @Version 1.0
+ */
+public class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode convertBST(TreeNode root) {
+        inTraverse(root);
+        return root;
+    }
+
+    int sum = 0;
+
+    public void inTraverse(TreeNode root) {
+        if (null == root) {
+            return;
+        }
+        inTraverse(root.right);
+        sum += root.val;
+        root.val = sum;
+        inTraverse(root.left);
+    }
+}
