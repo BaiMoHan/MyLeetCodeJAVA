9679912a59b38652f651038cbb82c46a36cdcd18
baimohan <csbaimohan@foxmail.com>
2021-03-02 16:25:00 +0800

    leetcode 0297

---
+++ 0297SerializeAndDeserializeBinaryTree/src/Codec.java
@@ -0,0 +1,27 @@
+/**
+ * 类描述：
+ *
+ * @ClassName Codec
+ * @Description TODO
+ * @Author BaiMohan
+ * @Date 2021/3/2 16:06
+ * @Version 1.0
+ */
+public class Codec {
+
+         public class TreeNode {
+    int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+
+    }
+}
