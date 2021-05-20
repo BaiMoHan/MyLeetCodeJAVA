2fc767103863f69b0a8ebf35977778ea34c38a99
baimohan <csbaimohan@foxmail.com>
2021-05-20 12:41:20 +0800

    leetcode 0692

---
+++ 0692TopKFrequentWords/src/Solution.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+/**
+ * 类描述：
+ *
+ * @ClassName Solution
+ * @Description leetcode 0692
+ * @Author BaiMohan
+ * @Date 2021/5/20 10:57
+ * @Version 1.0
+ */
+@SuppressWarnings("all")
+public class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String,Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            hashMap.put(words[i],hashMap.getOrDefault(words[i],0)+1);
+        }
+        PriorityQueue<Map.Entry<String,Integer>> queue = new PriorityQueue<>(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o1.getValue()== o2.getValue()?o2.getKey().compareTo(o1.getKey()): o1.getValue()-o2.getValue();
+            }
+        });
+        for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+            queue.offer(entry);
+            if(queue.size()>k){
+                queue.poll();
+            }
+        }
+        List<String> res = new ArrayList<>();
+        while (!queue.isEmpty()){
+            res.add(queue.poll().getKey());
+        }
+        Collections.reverse(res);
+        return res;
+    }
+}
