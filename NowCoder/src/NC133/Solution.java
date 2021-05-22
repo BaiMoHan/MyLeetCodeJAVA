05340de6445f383aff3bb4a87dab0e21aef48838
baimohan <csbaimohan@foxmail.com>
2021-05-22 09:10:41 +0800

    NC 133

---
+++ NowCoder/src/NC133/Solution.java
@@ -0,0 +1,38 @@
+package NC133;
+
+/**
+ * 类描述：
+ *
+ * @ClassName Solution
+ * @Description NC 133 链表的奇偶重排
+ * @Author BaiMohan
+ * @Date 2021/5/22 8:54
+ * @Version 1.0
+ */
+@SuppressWarnings("all")
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null) return null;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        while(even!=null&&even.next!=null){
+            odd.next=odd.next.next;
+            even.next=even.next.next;
+            odd=odd.next;
+            even= even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
